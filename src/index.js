72992f449493f3518d5ee06d9fc7ad9dfee5d805
frank pagan <frank@cocreate.app>
2021-09-02 01:55:58 +0000

    feat: functions to disableLinks and enableLinks

--- src/index.js
+++ src/index.js
@@ -13,102 +13,124 @@ const CoCreateLogic = {
 	getValues: getValues,
 };
 
-	function init() {
-		__initPassSessionIds(); // will be derprciated for CoCreate-localStorage
-		initLinks();
-	}
+const islink = new Map();
 
-	// ToDo: can be depreciated do to component localStorage
-	function __initPassSessionIds() {
-		let orgId = window.localStorage.getItem('organization_id');
-		let user_id = window.localStorage.getItem('user_id');
-		let adminUI_id = window.localStorage.getItem('adminUI_id');
-		let builderUI_id = window.localStorage.getItem('builderUI_id');
+function init() {
+	__initPassSessionIds(); // will be derprciated for CoCreate-localStorage
+	initLinks();
+}
 
-		__initPassItems(orgId, ".sessionOrg_Id", true);
-		__initPassItems(user_id, ".sessionUser_Id");
-		__initPassItems(adminUI_id, ".sessionAdminUI_Id");
-		__initPassItems(builderUI_id, ".sessionBuilderUI_Id");
-	}
+// ToDo: can be depreciated do to component localStorage
+function __initPassSessionIds() {
+	let orgId = window.localStorage.getItem('organization_id');
+	let user_id = window.localStorage.getItem('user_id');
+	let adminUI_id = window.localStorage.getItem('adminUI_id');
+	let builderUI_id = window.localStorage.getItem('builderUI_id');
 
-	// ToDo: can be depreciateddo to component localStorage
-	function __initPassItems(id, selector, noFetch) {
-		
-		if (id) {
-			let elements = document.querySelectorAll(selector);
-			elements.forEach(el => {
-				passAttributes._setAttributeValue(el, 'document_id', id);
-				passAttributes._setAttributeValue(el, 'fetch-document_id', id);
-				passAttributes._setAttributeValue(el, 'filter-value', id);
-			});
-		}
-	}
+	__initPassItems(orgId, ".sessionOrg_Id", true);
+	__initPassItems(user_id, ".sessionUser_Id");
+	__initPassItems(adminUI_id, ".sessionAdminUI_Id");
+	__initPassItems(builderUI_id, ".sessionBuilderUI_Id");
+}
 
-
-	function initLinks() {
-		document.addEventListener('click', linkEvent);
-	}
+// ToDo: can be depreciateddo to component localStorage
+function __initPassItems(id, selector, noFetch) {
 	
-	function linkEvent() {
-			const target = event.target.closest('[href], [target], [pass_to]');
-			if (!target) return;
-			if (target.hasAttribute('actions')) return;
-			if (target.closest('[actions]')) return;
-			runLink(target);
-	}
-	
-	function runLink(target) {
-		if (!target) return;
-		const href = target.getAttribute('href');
-		passAttributes._setPassAttributes(target);			
-
-		if (target.getAttribute('target') === 'modal') {
-			event.preventDefault();
-			if (typeof CoCreate.modal !== 'undefined') {
-				CoCreate.modal.open(target);
-			}
-			else if (href) {
-				openLink(target);
-			}
-		}
-		else if (href) {
-			event.preventDefault();
-			openLink(target);
-		}
-
-	}
-	
-	function openLink(link) {
-		var href = link.getAttribute('href');
-		var target = link.getAttribute('target');
-
-		if (target == "_blank") {
-			window.open(href, "_blank");
-		}
-		else if (target == "_window") {
-			window.open(href, '_blank', 'location=yes,height=570,width=520,scrollbars=yes,status=yes');
-		}
-		else {
-			window.open(href, "_self");
-		}
-	}
-	
-	function  disableLinks(btn) {
-		let Document = document;
-		let targetSelector = btn.getAttribute('link-target');
-		if (targetSelector) {
-			if(targetSelector.indexOf(';') !== -1) {
-				let documentSelector;
-				[documentSelector, targetSelector] = targetSelector.split(';');
-				let frame = document.querySelector(documentSelector);
-				Document = frame.contentDocument;
-			}
-		}
-		Document.removeEventListener("click", linkEvent);
-		Document.addEventListener("click", function(e){
-			e.preventDefault();
+	if (id) {
+		let elements = document.querySelectorAll(selector);
+		elements.forEach(el => {
+			passAttributes._setAttributeValue(el, 'document_id', id);
+			passAttributes._setAttributeValue(el, 'fetch-document_id', id);
+			passAttributes._setAttributeValue(el, 'filter-value', id);
 		});
 	}
+}
+
+
+function initLinks() {
+	document.logic = {islink: 'true'};
+	document.addEventListener('click', linkEvent, true);
+}
+
+function linkEvent() {
+		const target = event.target.closest('[href], [target], [pass_to]');
+		if (!target) return;
+		if (target.hasAttribute('actions')) return;
+		if (target.closest('[actions]')) return;
+		runLink(target);
+}
+
+
+function runLink(target) {
+	if (!target) return;
+	if (document.logic.link) return;
+	const href = target.getAttribute('href');
+	passAttributes._setPassAttributes(target);			
+
+	if (target.getAttribute('target') === 'modal') {
+		event.preventDefault();
+		if (typeof CoCreate.modal !== 'undefined') {
+			CoCreate.modal.open(target);
+		}
+		else if (href) {
+			openLink(target);
+		}
+	}
+	else if (href) {
+		event.preventDefault();
+		openLink(target);
+	}
+
+}
+
+function openLink(link) {
+	var href = link.getAttribute('href');
+	var target = link.getAttribute('target');
+
+	if (target == "_blank") {
+		window.open(href, "_blank");
+	}
+	else if (target == "_window") {
+		window.open(href, '_blank', 'location=yes,height=570,width=520,scrollbars=yes,status=yes');
+	}
+	else {
+		window.open(href, "_self");
+	}
+}
+
+function getDocument(btn) {
+	let Document = document;
+	let targetSelector = btn.getAttribute('link-target');
+	if (targetSelector) {
+		if(targetSelector.indexOf(';') !== -1) {
+			let documentSelector;
+			[documentSelector, targetSelector] = targetSelector.split(';');
+			let frame = document.querySelector(documentSelector);
+			Document = frame.contentDocument;
+		}
+	}
+	return Document;
+}
+
+function disableLinks(btn) {
+	let document = getDocument(btn);
+	document.logic = {link: 'false'};
+	document.removeEventListener("click", linkEvent, true);
+	document.addEventListener("click", preventDefault, true);
+}
+
+function enableLinks(btn) {
+	let document = getDocument(btn);
+	document.logic = {link: 'true'};
+	document.removeEventListener("click", preventDefault, true);
+	document.addEventListener("click", linkEvent, true);
+}
+
+function preventDefault(e) {
+	e.preventDefault();
+}
+
+	
 
 init();
 
@@ -152,6 +174,14 @@ action.init({
 	endEvent: "disableLinks",
 	callback: (btn, data) => {
 		disableLinks(btn);
+	}
+});
+
+action.init({
+	action: "enableLinks",
+	endEvent: "enableLinks",
+	callback: (btn, data) => {
+		enableLinks(btn);
 	}
 });
 
